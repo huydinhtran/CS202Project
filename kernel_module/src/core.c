873d7bb75d3881ef8482f9995fc848aad2f8c102
HuyTran <trandinhhuy99@gmail.com>
2022-03-10 16:09:55 -0800

    updated core

--- kernel_module/src/core.c
+++ kernel_module/src/core.c
@@ -49,25 +49,80 @@ extern struct miscdevice blockmma_dev;
 /**
  * Enqueue a task for the caller/accelerator to perform computation.
  */
+ 
+int getTask = 0;
+int syncTask = 0;
+int sendTask = 0;
+int compTask = 0;
+struct tasks{
+    struct blockmma_cmd cmd;
+    float *a;
+    float *b;
+    float *c;
+    int count;
+    struct tasks *next_task;
+};
+struct tasks *task_l;
+int tile_v = 0;
+
 long blockmma_send_task(struct blockmma_cmd __user *user_cmd)
 {
-    return 0;
+    printk(KERN_INFO "----------------blockMMA----------------");
+    printk(KERN_INFO "Send Task!!!");
+    if(sendTask == 0){
+         task_l = kmalloc(sizeof(struct tasks), GFP_KERNEL);  
+    }
+    struct tasks *temp_l;
+    temp_l = kmalloc(sizeof(struct tasks), GFP_KERNEL);;
+    temp_l->count = sendTask;
+    printk(KERN_INFO "Send Task1!!!");
+    printk(KERN_INFO "Send Task2!!!");
+    temp_l->a = kmalloc(128*128*sizeof(float *),GFP_KERNEL);
+    temp_l->b = kmalloc(128*128*sizeof(float *), GFP_KERNEL);
+    temp_l->c = kmalloc(128*128*sizeof(float *), GFP_KERNEL);
+    printk(KERN_INFO "Send Task3!!!");
+    copy_from_user(temp_l->a, (void *)user_cmd->a,128*128*sizeof(float *));
+    copy_from_user(temp_l->b, (void *)user_cmd->b,128*128*sizeof(float *));
+    if (copy_from_user(&temp_l->cmd, user_cmd, sizeof(task_l->cmd)))
+    {
+        return -1;
+    }
+    if(sendTask == 0){
+    	task_l = temp_l;
+    }
+    else{
+        struct tasks *temp_ll;
+        temp_ll = task_l;
+    	while(temp_ll->next_task){
+             temp_ll = temp_ll->next_task;
+    	}
+    	temp_ll->next_task = temp_l;
+    }
+    sendTask++;
+    struct task_struct *p = current;
+    return p->pid;
 }
 
-/**
- * Return until all outstanding tasks for the calling process are done
- */
-int blockmma_sync(struct blockmma_cmd __user *user_cmd)
-{
-    return 0;
-}
 
 /**
  * Return the task id of a task to the caller/accelerator to perform computation.
  */
 int blockmma_get_task(struct blockmma_hardware_cmd __user *user_cmd)
 {
-    return 0;
+    if(getTask<sendTask){
+        printk(KERN_INFO "Entered Get Task!!!");
+        struct tasks *temp_l = task_l;
+        while(temp_l->count != getTask){
+        	temp_l = temp_l->next_task;
+        }
+    	copy_to_user((void *)user_cmd->a, temp_l->a, 128*128*sizeof(float *));
+    	copy_to_user((void *)user_cmd->b, temp_l->b, 128*128*sizeof(float *));
+    	copy_to_user((void *)user_cmd->c, temp_l->c, 128*128*sizeof(float *));
+    	getTask++;
+    	struct task_struct *p = current;
+    	return p->pid;
+    }
+    return -1;
 }
 
 
@@ -76,8 +131,43 @@ int blockmma_get_task(struct blockmma_hardware_cmd __user *user_cmd)
  */
 int blockmma_comp(struct blockmma_hardware_cmd __user *user_cmd)
 {
-    return 0;
+	printk(KERN_INFO "Entered Comp Task!!!");
+	struct tasks *temp_l;
+	temp_l = task_l;
+        while(temp_l->count != compTask){
+        	temp_l = temp_l->next_task;
+        }
+        temp_l->c = kmalloc(128*128*sizeof(float *), GFP_KERNEL);
+	if (copy_from_user(temp_l->c, (void *)user_cmd->c, 128*128*sizeof(float *)))
+    	{
+        	return -1;
+    	}
+    	compTask++;
+    	struct task_struct *p = current;
+    	printk(KERN_INFO "%d\n", p->pid);
+    	return p->pid;
 }
+
+
+/**
+ * Return until all outstanding tasks for the calling process are done
+ */
+int blockmma_sync(struct blockmma_cmd __user *user_cmd)
+{    
+    if(sendTask == compTask){
+	printk(KERN_INFO "Check Sync Task!!!");
+//	while(task_l->next_task){
+	//	printk(KERN_INFO "Final: %d\n", task_l->count);
+	//	task_l = task_l->next_task;
+	//}
+	copy_to_user((void *)task_l->cmd.c, task_l->c, 256*256*sizeof(float *));
+    	struct task_struct *p = current;
+    	printk(KERN_INFO "%d\n", p->pid);
+    	return 0;
+    }
+    return -1;
+}
+
 
 /*
  * Tell us who wrote the module
@@ -85,12 +175,12 @@ int blockmma_comp(struct blockmma_hardware_cmd __user *user_cmd)
 int blockmma_author(struct blockmma_hardware_cmd __user *user_cmd)
 {
     struct blockmma_hardware_cmd cmd;
-    char authors[] = "Yu-Chia Liu (yliu719), 987654321 and Hung-Wei Tseng (htseng), 123456789";
+    char auth[] = "Huy Dinh Tran (htran197), 862325308 and Suraj Thalari (sthal001), 862325121";
     if (copy_from_user(&cmd, user_cmd, sizeof(cmd)))
     {
         return -1;
     }
-    copy_to_user((void *)cmd.a, (void *)authors, sizeof(authors));
+    copy_to_user((void *)cmd.a, (void *)auth, sizeof(auth));
     return 0;
 }
 
@@ -111,4 +201,3 @@ void blockmma_exit(void)
     printk("BlockMMA removed\n");
     misc_deregister(&blockmma_dev);
 }
-
